e042b6b955611808f98f5c85b2ef9942022ea8b6
Jose Benitez <jose@intuitivo.ai>
2023-03-04 13:33:28 -0500

    minor fix

--- chatgpt.py
+++ chatgpt.py
@@ -12,20 +12,19 @@ def get_response(incoming_msg):
     if incoming_msg == "clear":
         data.clear()
         data.append({"role": "assistant", "content": 'hello'})
-        
-    
-    data.append({"role": "assistant", "content": incoming_msg})
-    print('THE DATA', data)
-    messages = [ systemPrompt ]
-    messages.extend(data)
-    print('THE MESSAGES', messages)
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages
-        )
-        content = response["choices"][0]["message"]["content"]
-        return content
-    except openai.error.RateLimitError as e:
-        print(e)
-        return ""
+    else:  
+        data.append({"role": "assistant", "content": incoming_msg})
+        print('THE DATA', data)
+        messages = [ systemPrompt ]
+        messages.extend(data)
+        print('THE MESSAGES', messages)
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages
+            )
+            content = response["choices"][0]["message"]["content"]
+            return content
+        except openai.error.RateLimitError as e:
+            print(e)
+            return ""
